4c65eea50da1632f9794b2adae073cb007a5775b
superpck <superpck@gmail.com>
2025-09-19 09:44:19 +0700

    แยก route server

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import indexRoute from './indexRoute';
 import loginRoute from './loginRoute';
+import serverRoute from './serverRoute';
 
 const router = express.Router();
 
 // Use routes
 router.use('/', indexRoute);
+router.use('/server', serverRoute);
 router.use('/auth', loginRoute);
 
 export default router;
